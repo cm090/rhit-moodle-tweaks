d93e2727b05aec0de8d6c63c72868cefc815dc23
Canon Maranda <22649817+cm090@users.noreply.github.com>
2022-12-09 19:22:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# rhit-moodle-tweaks
+# Moodle Tweaks
 CSS and JS files that add functionality to Moodle
+
+Note: This code was designed for Rose-Hulman's Moodle site
+
+
+## Getting Started
+1. Download a code injector plugin for your browser. I'm using [Injector for Chrome](https://chrome.google.com/webstore/detail/injector/bfdonckegflhbiamlmidciapolfccmmb).
+2. Create new CSS and JS snippets for the website domain. Copy and paste the code from `main.css` and  `main.js`.
+3. Click save and reload your Moodle page
+
+## Features
+### JavaScript
+CleanSideMenu:
+- Removes old courses from the sidebar. Click on "My courses" to set up
+
+ModifyURL:
+- Redirects to dashboard when on site home and course enrollment pages. Add `#bypass` to the end of the URL to avoid redirect.
+- Removes forcedownload attributes from file links. Useful for images and PDFs.
+
+### CSS
+A modern interface design with darker colors
+
+Screenshots:
+
+![Dashboard screen](screenshots/dashboard.png)
---
+++ main.js
@@ -0,0 +1,31 @@
+const cleanSideMenu = () => {
+    let quarter = localStorage.getItem('filterQuarter');
+    let start = false;
+    document.querySelectorAll("#nav-drawer > nav.list-group > ul > li").forEach(item => {
+        item.style.display = '';
+        let text = item.querySelector('.media-body').innerText;
+        if (!start && text == 'My courses') {
+            start = true;
+            item.onclick = () => {
+                let newQuarter = prompt('Select a quarter (Ex: 2223F):');
+                newQuarter = parseInt(newQuarter.substring(0, 4)) + newQuarter.charAt(4);
+                localStorage.setItem('filterQuarter', newQuarter);
+                cleanSideMenu();
+            }
+            return;
+        } else if (start && !text.includes(quarter))
+            item.style.display = 'none';
+    });
+}
+const modifyURL = () => {
+    if ((window.location.pathname.length < 2 || window.location.href.includes('enrol')) && !window.location.hash.includes('bypass'))
+        window.location.pathname = '/my';
+    document.querySelectorAll('a').forEach((link) => {
+        if (link.href.includes('?forcedownload=1')) {
+            link.href = link.href.split('?forcedownload')[0];
+            link.target = '_blank';
+        }
+    });
+}
+cleanSideMenu();
+modifyURL();
