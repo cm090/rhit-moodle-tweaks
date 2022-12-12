36f611a1485169e5bfe2a4ed288321dce150ccec
Canon Maranda <22649817+cm090@users.noreply.github.com>
2022-12-12 14:42:49 -0500

    Update styling for cleanSideMenu

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const cleanSideMenu = () => {
     let quarter = localStorage.getItem('filterQuarter');
     let start = false;
+    const activeCourse = (document.querySelector('[data-key="coursehome"] .media-body')) ? document.querySelector('[data-key="coursehome"] .media-body').innerText : '';
     document.querySelectorAll("#nav-drawer > nav.list-group > ul > li").forEach(item => {
         item.style.display = '';
         let text = item.querySelector('.media-body').innerText;
-        if (!start && text == 'My courses') {
-            start = true;
-            item.onclick = () => {
-                let newQuarter = prompt('Select a quarter (Ex: 2223F):');
-                newQuarter = parseInt(newQuarter.substring(0, 4)) + newQuarter.charAt(4);
-                localStorage.setItem('filterQuarter', newQuarter);
-                cleanSideMenu();
-            }
-            return;
+        if (!start) {
+            if (text == 'My courses') {
+                start = true;
+                item.onclick = () => {
+                    let newQuarter = prompt('Select a quarter (Ex: 2223F):');
+                    newQuarter = parseInt(newQuarter.substring(0, 4)) + newQuarter.charAt(4);
+                    localStorage.setItem('filterQuarter', newQuarter);
+                    cleanSideMenu();
+                }
+                return;
+            } else if (item.querySelector('a').href == window.location.href)
+                item.querySelector('a').classList.add('active');
         } else if (start && !text.includes(quarter))
             item.style.display = 'none';
+        else if (text == activeCourse)
+            item.querySelector('a').classList.add('active');
     });
 }
 const modifyURL = () => {
